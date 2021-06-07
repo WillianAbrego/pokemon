5176b2a0d4122f593d2b8248a0538619192168a6
WillianAbrego <willianantonioabrego@gmail.com>
2021-06-07 06:43:13 -0600

    funcionalidad paginacion, sin modificar contenido

--- docs/pokemon2.js
+++ docs/pokemon2.js
@@ -111,3 +111,14 @@ $(document).on("click", "button.btn", function (e) {
   // console.log(title);
 });
 //document.getElementsByClassName("bg-white")[0].childNodes[2].innerText
+
+$("#pagination-demo").twbsPagination({
+  totalPages: 16,
+  visiblePages: 6,
+  next: "Next",
+  prev: "Prev",
+  onPageClick: function (event, page) {
+    //fetch content and render here
+    $("#page-content").text("Page " + page) + " content here";
+  },
+});
